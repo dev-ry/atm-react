4c66d0e2875ffc3694472aa2b323c0a43f2950c1
Devin Ryan <devin.ryan7@gmail.com>
2017-10-26 21:40:13 -0500

    added logic

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        balance: 0
+      }
+  }
+
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
+        <h2>{this.props.Account}</h2>
+          let balanceClass = 'balance';
+          if (this.state.balance === 0) {
+            balanceClass += ' zero';
+          }
+        <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
+        <input type="button" value="Deposit" onClick={(e) => this.handleDepositClick(e)} />
+          handleDepositClick(e) {
+            e.preventDefault();
+            let amount = this.inputBox.value;
+            let newBalance = this.state.balance + amount;
+            this.setState({
+              balance: newBalance
+            })
+            this.inputBox.value = '';
+          }
         <input type="button" value="Withdraw" />
+          handleDepositClick(e) {
+            e.preventDefault();
+            let amountWithdrawn = this.inputBox.value;
+            let balanceRemaining = this.state.balance - amountWithdrawn;
+            this.setState({
+              balance: newBalance
+            })
+            this.inputBox.value = '';
+          }
       </div>
     )
   }
